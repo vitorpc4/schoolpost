3ec1bbda10422d3d9d7fd352871789b284e2391b
Vitor <vitorpc4@hotmail.com>
2024-09-22 21:17:28 -0300

    Refactor: Atualizar importação de TypeUser, esquemas de validação, métodos de usuário e estrutura do módulo de usuário.
    - Atualizar user.controller.ts para modificar a declaração de importação de TypeUser, ajustar esquemas de validação, refatorar métodos de criação e atualização de usuário, e remover lógica desnecessária relacionada a escola.
    - Refatorado o módulo de usuário para fornecer apenas o UsersService como provider.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,12 +5,10 @@ import { Post } from '@/entities/models/post.entity';
 import { User } from '@/entities/models/user.entity';
 import { School } from '@/entities/models/school.entity';
 import { UsersService } from '@/services/user.service';
-import { SchoolsService } from '@/services/school.service';
-import { PostsService } from '@/services/post.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post, User, School])],
-  providers: [UsersService, SchoolsService, PostsService],
+  providers: [UsersService],
   controllers: [UserController],
   exports: [UsersService]
 })
