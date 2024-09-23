ce06e1a1946414f5be6826fbc94b3200ed5e7dae
Vitor <vitorpc4@hotmail.com>
2024-09-22 21:20:02 -0300

    feat: Adicionar tipo genérico para mockear objetos, listar diretório Mock e refatorar métodos do PostService.
    - Criar um novo arquivo TypeScript definindo um tipo genérico para mockear objetos usando Jest.
    - Adicionar o diretório "test/Mock" à lista de diretórios de módulos para testes Jest.
    - Refatorados os métodos do PostService para lidar com criação, atualização, busca por palavra-chave, associação, rascunhos e exclusão de posts de forma eficiente.

--- test/test/posts.service.e2e-spec.ts
+++ test/test/posts.service.e2e-spec.ts
@@ -1,87 +1,41 @@
 import { IPost } from "@/entities/interfaces/posts.interface";
-import { ISchool } from "@/entities/interfaces/school.interface";
-import { IUser } from "@/entities/interfaces/user.interface";
 import { Post } from "@/entities/models/post.entity";
-import { School } from "@/entities/models/school.entity";
-import { TypeUser, User } from "@/entities/models/user.entity";
-import schoolFactory from "@/seeders/factories/school.factory";
-import userFactory from "@/seeders/factories/user.factory";
-import PostSeeder from "@/seeders/post.seeder";
-import SchoolSeeder from "@/seeders/school.seeder";
-import UserSeeder from "@/seeders/user.seeder";
 import { PostsService } from "@/services/post.service";
-import { SchoolsService } from "@/services/school.service";
-import { UsersService } from "@/services/user.service";
 import { Test, TestingModule } from "@nestjs/testing";
-import { getRepositoryToken, TypeOrmModule } from "@nestjs/typeorm";
+import { getRepositoryToken } from "@nestjs/typeorm";
 import { log } from "console";
-import { before } from "node:test";
-import { delay } from "rxjs";
-import { DataSource, DataSourceOptions, Repository } from "typeorm";
-import { createDatabase, dropDatabase, dropPostgresDatabase, runSeeders, SeederOptions } from "typeorm-extension";
+import { Repository } from "typeorm";
+import { TypeUser } from "@/entities/models/userSchoolAssociation.entity";
+import { MockType } from "test/Mock/Type.Mock";
+
+
+const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(() => ({
+    findOneBy: jest.fn(entity => entity),
+    save: jest.fn(entity => entity),
+    delete: jest.fn(entity => entity),
+    createQueryBuilder: jest.fn(() => ({
+        where: jest.fn().mockReturnThis(),
+        andWhere: jest.fn().mockReturnThis(),
+        getMany: jest.fn().mockReturnValue([]),
+        getOne: jest.fn().mockReturnValue({}),
+    })),
+  }));
+
 
 describe('PostService', () => {
-    const configData : DataSourceOptions & SeederOptions = {
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        database: 'test',
-        username: 'root',
-        password: 'password',
-        entities: [Post,User,School],
-        synchronize: true,
-        dropSchema: true,
-        seeds: [SchoolSeeder, UserSeeder, PostSeeder],
-        factories: [schoolFactory, userFactory],
-        migrationsRun: true,
-    }
-
     let postService: PostsService;
-    let userService: UsersService;
-    let schoolService: SchoolsService;
-    let repositoryUser : Repository<User>;
-    let repositorySchool : Repository<School>;
-    let repositoryPost : Repository<Post>;
-
-    let module: TestingModule;
-
-
-    before(async () => {
-        await dropDatabase({
-            ifExist: true,
-            options: configData
-        })
-
-        await createDatabase({
-            ifNotExist: true,
-            options: configData
-        });
-
-        const dataSource = new DataSource(configData);
-        await dataSource.initialize();
-
-        await runSeeders(dataSource);
-
-        if (dataSource.isInitialized) await dataSource.destroy();
-    })
+    let repositoryMock: MockType<Repository<Post>>;
 
     beforeAll(async () => {
-        module = await Test.createTestingModule({
-            imports: [
-                TypeOrmModule.forRoot(configData),
-                
-                TypeOrmModule.forFeature([Post, User, School])
-            ],
-            providers: [PostsService, UsersService, SchoolsService]
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            providers: [
+                PostsService,
+                { provide: getRepositoryToken(Post), useFactory: repositoryMockFactory }
+            ]
         }).compile();
         
-    
-        postService = module.get<PostsService>(PostsService);
-        userService = module.get<UsersService>(UsersService);
-        schoolService = module.get<SchoolsService>(SchoolsService);
-        repositoryPost = module.get<Repository<Post>>(getRepositoryToken(Post));
-        repositoryUser = module.get<Repository<User>>(getRepositoryToken(User));
-        repositorySchool = module.get<Repository<School>>(getRepositoryToken(School));
+        postService = moduleFixture.get<PostsService>(PostsService);
+        repositoryMock = moduleFixture.get(getRepositoryToken(Post));
     });
 
     it('deve trazer um post', async () => {
@@ -109,41 +63,219 @@ describe('PostService', () => {
         })
         
         const result = await postService.findById(id);
-        
-
-        log(result);
 
         expect(result.id).toEqual(id);
     })
 
-    it('Solicitação de todos os posts', async () => {
-        const result = await postService.findAll(1, 10);
-        expect(result.length).toBeGreaterThanOrEqual(1);
-    });
+    it('deve trazer os posts por palavra chave', async () => {
+        const page: number = 1;
+        const limit: number = 10;
+        const associateIds: number[] = [1];
+        const search: string = "Teste";
 
-    it('Criação de post', async () => {
-        const getSchool = await schoolService.findAll(1, 10);
-        const getUser = await userService.findById(1);
+        postService.findPostByKeyWord = jest.fn().mockReturnValueOnce([
+            {
+                id: 1,
+                title: "Teste",
+                content: "Teste",
+                isDraft: false,
+                status: true,
+            },
+            {
+                id: 2,
+                title: "Teste",
+                content: "Teste",
+                isDraft: false,
+                status: true,
+            },
+            {
+                id: 3,
+                title: "Teste",
+                content: "Teste",
+                isDraft: false,
+                status: true,
+            }
+        ]);
 
+        const result = await postService.findPostByKeyWord(associateIds, search, limit, page);
 
-        getUser.schools = [getSchool[0]];
-        
-        const r = await userService.update(getUser);
-
-        const post:IPost = {
-            content: 'Teste de conteúdo',
-            title: 'Teste de título',
-            isDraft: false,
-            school: r.schools[0],
-            status: true,
-            user: r,
-        }
-
-        const result = await postService.create(post);
-
-        expect(result).toHaveProperty('id');
-
+        expect(result.length).toBeGreaterThanOrEqual(3);
     })
 
+    it('deve trazer todos os rascunhos', async () => {
+        const page: number = 1;
+        const limit: number = 10;
+        const associateIds: number[] = [1];
+
+        postService.findDraftsByAssociationId = jest.fn().mockReturnValueOnce([
+            {
+                id: 1,
+                title: "Teste",
+                content: "Teste",
+                isDraft: true,
+                status: true,
+            },
+            {
+                id: 2,
+                title: "Teste",
+                content: "Teste",
+                isDraft: true,
+                status: true,
+            },
+            {
+                id: 3,
+                title: "Teste",
+                content: "Teste",
+                isDraft: true,
+                status: true,
+            }
+        ]);
+
+        const result = await postService.findDraftsByAssociationId(associateIds, page, limit);
+
+        expect(result.length).toBeGreaterThanOrEqual(3);
+    })
+
+    it('deve trazer todos os posts de uma escola', async () => {
+        const page: number = 1;
+        const limit: number = 10;
+        const associateIds: number[] = [1];
+
+        postService.findPostByAssociationId = jest.fn().mockReturnValueOnce([
+            {
+                id: 1,
+                title: "Teste",
+                content: "Teste",
+                isDraft: false,
+                status: true,
+            },
+            {
+                id: 2,
+                title: "Teste",
+                content: "Teste",
+                isDraft: false,
+                status: true,
+            },
+            {
+                id: 3,
+                title: "Teste",
+                content: "Teste",
+                isDraft: false,
+                status: true,
+            }
+        ]);
+
+        const result = await postService.findPostByAssociationId(associateIds, page, limit);
+
+        expect(result.length).toBeGreaterThanOrEqual(3);
+    })
+
+    it('Criação de post', async () => {
+        const post: IPost = {
+            title: "Teste",
+            content: "TesteContent",
+            isDraft: false,
+            status: true,
+            id: 1,
+            userSchoolAssociation: {
+                id: 1,
+                status: true,
+                typeUser: TypeUser.Professor,
+                admin: true,
+                school: {
+                    name: "Teste",
+                    status: true,
+                },
+                user: {
+                    email: "teste",
+                    password: "teste",
+                    status: true,
+                    username: "teste"
+                }
+            },
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }
+
+        postService.create = jest.fn().mockReturnValueOnce({
+            title: "Teste",
+            content: "TesteContent",
+            isDraft: false,
+            status: true,
+            userSchoolAssociation: {
+                id: 1,
+                status: true,
+                typeUser: "Professor",
+                admin: true,
+            },
+            id: 1,
+            createAt: new Date(),
+            updateAt: new Date(),
+        });
+        
+        const result = await postService.create(post)
+        
+        expect(result.title).toEqual(post.title);
+    })
+
+    it('Deve atualizar o post', async () => {
+        const post: IPost = {
+            title: "TesteAtualiza",
+            content: "TesteContent",
+            isDraft: false,
+            status: true,
+            id: 1,
+            userSchoolAssociation: {
+                id: 1,
+                status: true,
+                typeUser: TypeUser.Professor,
+                admin: true,
+                school: {
+                    id: 'acb91924-c9c6-4efa-9209-b6c86e144c80',
+                    status: true,
+                    name: "Teste",
+                },
+                user: {
+                    email: "teste",
+                    password: "teste",
+                    status: true,
+                    username: "teste"
+                }
+            },
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }
+
+        postService.update = jest.fn().mockReturnValueOnce({
+            title: "TesteAtualiza",
+            content: "TesteContentAtualizada",
+            isDraft: false,
+            status: true,
+            userSchoolAssociation: {
+                id: 1,
+                status: true,
+                typeUser: "Professor",
+                admin: true,
+            },
+            id: 1,
+            createAt: new Date(),
+            updateAt: new Date(),
+        });
+
+
+        const result = await postService.update(post);
+
+        expect(result.title).toEqual(post.title);
+    })
+
+    it('Deve deletar um post', async () => {
+        const id: number = 1;
+
+        postService.delete = jest.fn().mockReturnValueOnce(true);
+
+        const result = await postService.delete(id);
+
+        expect(result).toBeTruthy();
+    })
 });
 
